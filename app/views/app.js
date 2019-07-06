f81c7e366b79894a14a21e704e7fd03720569caf
junxiachen <junxiachen@chenjunxiadeAir.lan>
2019-07-06 15:44:28 +0800

    fix: 局部路由

--- app/views/app.js
+++ app/views/app.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import PrivateRoute from '../components/privateRoute/privateRoute'
 import Login from './login/login'
 import Dashboard from './dashboard/dashboard'
 import PageNotFound from './pageNotFound/pageNotFound'
 
-function App() {
+function App () {
   return (
     <Router>
       <Switch>
         <Route exact path="/login" component={Login} />
         <PrivateRoute exact path="/" component={Dashboard} />
-        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/admin" component={Dashboard} />
+        <Route exact path="/404" component={PageNotFound} />
         <Route component={PageNotFound} />
       </Switch>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- app/views/dashboard/dashboard.js
+++ app/views/dashboard/dashboard.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Route, Switch, Link, Redirect } from 'react-router-dom'
+import style from './dashboard.css'
 
-function Dashboard() {
+function Dashboard ({ match }) {
   return (
-    <div>Dashboard</div>
-  );
+    <div>
+      <div className={style.layout}>
+        <div className={style.aside}>
+          <h1>side bar</h1>
+          <div><Link to="/admin/withdraw">提现管理</Link></div>
+          <div><Link to="/admin/about">关于</Link></div>
+        </div>
+        <div className={style.main}>
+          <Switch>
+            <Route exact path={`${match.path}/withdraw`} render={() => <h1>提现管理页面</h1>} />
+            <Route exact path={`${match.path}/about`} render={() => <h1>关于页面</h1>} />
+            <Redirect to="/404" />
+          </Switch>
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
 
